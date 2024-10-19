7b5c08cbbf7b67789510dc2a9d609437a944a135
Rohit Nair <rohitnair099@outlook.com>
2024-10-19 16:11:38 +0100

    First commit

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
