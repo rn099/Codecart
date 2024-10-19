632460ac2334e8efb29272ac6f536f97dbba199b
Rohit Nair <rohitnair099@outlook.com>
2024-10-19 19:08:52 +0100

    added routes and navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../assets/cc-logo.png";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>CodeCart</h1>
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={logo} className="w-36" alt="logo" />
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <li></li>
+      </ul>
     </div>
   );
 };
